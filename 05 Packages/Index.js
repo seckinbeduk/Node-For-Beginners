3ef77ab2def75efe5eb781f095ef7612f1aa9b84
seckinbeduk <seckinbeduk@gmail.com>
2016-10-20 23:58:53 +0300

    Add package.json file and using npm start

---
+++ 05 Packages/Index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+
+//const configJson = fs.readFileSync("./config.json");
+
+fs.readFile('./config.json', 'utf8', function (err, data) {
+
+    const config = JSON.parse(data);
+
+    const app = express();
+
+    app.use(express.static(config.webServer.folder));
+
+    const httpServer = http.createServer(app);
+
+    httpServer.listen(config.webServer.port, function (err) {
+
+        if (err) {
+            console.log(err.message);
+            return;
+        }
+
+        console.log('web server listening on port %d', config.webServer.port);
+    });
+
+
+});
+
+console.log('reading config file');
+
+// const config = JSON.parse(configJson);
+
+// const app = express();
+
+// app.use(express.static(config.webServer.folder));
+
+// const httpServer = http.createServer(app);
+
+// httpServer.listen(config.webServer.port, function(err){
+
+//     if(err){
+//         console.log(err.message);
+//         return;
+//     }
+
+//     console.log('web server listening on port %d',config.webServer.port);
+// })
